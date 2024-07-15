c0bb8129200a02ba1952063c69fc9a0898f03c4a
pr-citrate <pr.citrate@gmail.com>
2024-07-16 04:04:20 +0900

    Update: Make font size bigger

--- app.py
+++ app.py
@@ -6,7 +6,31 @@ from streamlit_folium import st_folium
 
 
 def main() -> None:
-    st.write("# 무더위쉼터 정보")
+    st.set_page_config(layout="wide")
+    st.markdown(
+        """
+    <style>
+        h1 {
+            font-size: 4.5rem !important;
+        }
+
+        h2 {
+            font-size: 3.5rem !important;
+        }
+
+        p {
+            font-size: 2.5rem !important;
+        }
+
+        input[type="text"] {
+            font-size: 1.5rem !important;
+        }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+    st.title("무더위쉼터 정보")
 
     st.write("---")
     st.write("## 검색하기")
@@ -42,12 +66,12 @@ def main() -> None:
         map_df: pd.DataFrame = df[["이름", "위도", "경도"]]
         display_df: pd.DataFrame = df.drop(columns=["위도", "경도"])
 
-        center: list[float, float] = [36.01457166, 127.0347845]
+        center: list[float, float] = [map_df["위도"].mean(), map_df["경도"].mean()]
         map: folium.Map = folium.Map(location=center, zoom_start=13)
 
         for i in range(len(map_df)):
             popup_info: str = f"""
-            <div style="font-size: 16px;">
+            <div style="font-size: 2.5rem;">
                 <b>이름:</b> {map_df["이름"][i]}<br>
                 <b>주소:</b> {df["주소"][i]}<br>
                 <b>시설 종류:</b> {df["시설 종류"][i]}<br>
@@ -56,11 +80,17 @@ def main() -> None:
             folium.Marker(
                 location=[map_df["위도"][i], map_df["경도"][i]],
                 popup=folium.Popup(popup_info, max_width=300),
-                tooltip=map_df["이름"][i],
+                # tooltip=map_df["이름"][i],
+                icon=folium.Icon(color="blue", icon="info-sign"),
             ).add_to(map)
 
+        display_df.index += 1
+
         st_folium(map, width=700, height=500)
-        st.write(display_df)
+        styled_df: pd.DataFrame = display_df.style.set_table_styles(
+            [{"selector": "th, td", "props": [("font-size", "1.5rem")]}]
+        )
+        st.write(styled_df.to_html(), unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
